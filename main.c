1affee361ac6e42d854fde624e4f02fbb518b372
addirenea <98353646+addirenea@users.noreply.github.com>
2022-04-26 13:53:01 -0400

    we done.

--- main.c
+++ main.c
@@ -7,173 +7,162 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+int main(int argc, char *argv[]) {
 
-void batchMode() {
+    // more than one file
+    if (argc > 2) {
+        exit(1);
+    }
 
-    printf("batch mode under construction \n");
+    else {
 
-}
+        // initalize path variable
+        int pathsc = 1;
+        char paths[512][1024];
+        char *starting_path = "/bin";
+        strcpy(*paths, starting_path);
 
+        int needToWait = 0; // flag for forked processes
 
-void interactiveMode() {
+        size_t bufsize = 1024;
+        char *input = (char *)malloc(bufsize * sizeof(char));
 
-    // initalize path variable
-    int pathsc = 1;
-    char paths[512][1024];
-    char *starting_path = "/bin";
-    strcpy(*paths, starting_path);
+        FILE *fp;
+        if (argc == 2) {
+            fp = fopen(argv[1], "r");
+        }
 
-    int needToWait = 0; // flag for forked processes
+        int line_rc;
 
-    size_t bufsize = 1024;
-    char *input = (char *)malloc(bufsize * sizeof(char));
+        while (1) {
 
+            if (argc == 1) { // interactive mode
+                printf("\033[1;35m");
+                printf("ccsh> ");
+                printf("\033[0m");
+                line_rc = getline(&input, &bufsize, stdin);
+            }
 
-    while (1) {
-        // gets next line of input
-        printf("\033[1;35m");
-        printf("ccsh> ");
-        printf("\033[0m");
-        getline(&input, &bufsize, stdin);
-        // IDEA: i think that for batch mode we would just need to tweak here
-        // where is says stdin for getline
-
-
-        if (strcmp(input, "\n") != 0) { // prevent segfault when user enters nothing
-
-            // split input into seperate command prompts
-            int cmdc = getCmdCount(input);
-            char *commands[cmdc];
-
-            char *tmp_cmd = strtok (input, "&");
-            for (int i = 0; i < cmdc; i++) {
-
-                commands[i] = tmp_cmd;
-                tmp_cmd = strtok (NULL, "&");
-
+            else { // batch mode
+                line_rc = getline(&input, &bufsize, fp);
             }
 
 
-            // for each command...
-            pid_t child_pid;
+            if (strcmp(input, "\n") != 0) { // prevent segfault when user enters nothing
 
-            for (int i = 0; i < cmdc; i++) {
+                // split input into seperate command prompts
+                int cmdc = getCmdCount(input);
+                char *commands[cmdc];
 
-                // construct args[]
-                int argc = getWordCount(commands[i]);
-                char *args[argc + 1];
+                char *tmp_cmd = strtok (input, "&");
+                for (int i = 0; i < cmdc; i++) {
 
-                char *arg = strtok (commands[i], " \t\n");
-                for (int i = 0; i < argc; i++) {
-
-                    args[i] = arg;
-                    arg = strtok (NULL, " \t\n");
+                    commands[i] = tmp_cmd;
+                    tmp_cmd = strtok (NULL, "&");
 
                 }
 
-                args[argc] = NULL;
+
+                // for each command...
+                pid_t child_pid;
+
+                for (int i = 0; i < cmdc; i++) {
+
+                    // construct args[]
+                    int arg_ct = getWordCount(commands[i]);
+                    char *args[arg_ct + 1];
+
+                    char *arg = strtok (commands[i], " \t\n");
+                    for (int i = 0; i < arg_ct; i++) {
+
+                        args[i] = arg;
+                        arg = strtok (NULL, " \t\n");
+
+                    }
+
+                    args[arg_ct] = NULL;
 
 
-                // check for built in commands
-                if (strcmp(args[0], "exit") == 0) {
+                    // check for built in commands
+                    if (strcmp(args[0], "exit") == 0 || line_rc == -1) {
 
-                    free(input);
-                    exit(0);
+                        free(input);
+                        exit(0);
 
-                }
+                    }
 
-                else if (strcmp(args[0], "cd") == 0) {
+                    else if (strcmp(args[0], "cd") == 0) {
 
-                    char dirpath[1000];
+                        char dirpath[1000];
 
-                    if (argc <= 1 || chdir(args[1]) != 0) {
-                        errorOccurred();
+                        if (arg_ct <= 1 || chdir(args[1]) != 0) {
+                            errorOccurred();
+                        }
+
+                        else {
+                            printf("cwd: %s\n", getcwd(dirpath, 1000));
+                        }
+
+                    }
+
+                    else if (strcmp(args[0], "path") == 0) {
+
+                        pathsc = arg_ct - 1;
+
+                        for (int i = 0; i < pathsc; i++) {
+
+                            strcpy(*(paths + i), args[i + 1]);
+
+                        }
+
+                    }
+
+                    else if (strcmp(args[0], "seepath") == 0) {
+
+                        for (int i = 0; i < pathsc; i++) {
+
+                            printf("[%s]\n", paths[i]);
+
+                        }
+
                     }
 
                     else {
-                        printf("cwd: %s\n", getcwd(dirpath, 1000));
-                    }
 
-                }
+                        needToWait = 1;
+                        child_pid = fork();
 
-                else if (strcmp(args[0], "path") == 0) {
+                        // child runs command
+                        if (child_pid == 0) {
 
-                    pathsc = argc - 1;
+                            execCommand(args, paths, pathsc);
 
-                    for (int i = 0; i < pathsc; i++) {
+                        }
 
-                        strcpy(*(paths + i), args[i + 1]);
+                        // error forking
+                        else if (child_pid < 0) {
 
-                    }
+                            errorOccurred();
 
-                }
-
-                // FOR TESTING PURPOSES, REMOVE BEFORE SUBMISSION
-                else if (strcmp(args[0], "seepath") == 0) {
-
-                    for (int i = 0; i < pathsc; i++) {
-
-                        printf("[%s]\n", paths[i]);
+                        }
 
                     }
 
                 }
 
-                else {
+                // parent waits for all children processes to finish
+                if (needToWait == 1) {
 
-                    needToWait = 1;
-                    child_pid = fork();
-
-                    // child runs command
-                    if (child_pid == 0) {
-
-                        execCommand(args, paths, pathsc);
-
-                    }
-
-                    // error forking
-                    else if (child_pid < 0) {
-
-                        errorOccurred();
-
-                    }
+                    pid_t wait_pid;
+                    int status = 0;
+                    while ((wait_pid = wait(&status)) > 0);
+                    needToWait = 0;
 
                 }
-
-            }
-
-            // parent waits for all children processes to finish
-            if (needToWait == 1) {
-
-                pid_t wait_pid;
-                int status = 0;
-                while ((wait_pid = wait(&status)) > 0);
-                needToWait = 0;
-                
             }
         }
-    }
 
-    free(input);
-
-}
-
-
-int main(int argc, char *argv[]) {
-
-    // no file given -> interactive mode
-    if (argc == 1) {
-        interactiveMode();
-    }
-
-    // one file given -> batch mode
-    else if (argc == 2) {
-        batchMode();
-    }
-
-    // more than one file
-    else {
-        exit(1);
+        free(input);
     }
 
     exit(0);
